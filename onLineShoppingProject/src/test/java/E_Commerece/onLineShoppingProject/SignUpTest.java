34238fbb3959e18c5ff4c645d47ef985be11f7ab
Owner <manvir.singh.parmar@gmail.com>
2020-05-14 13:27:11 -0400

    Data driven testing functionality added using JDBC connection and oracle database.

--- onLineShoppingProject/src/test/java/E_Commerece/onLineShoppingProject/SignUpTest.java
+++ onLineShoppingProject/src/test/java/E_Commerece/onLineShoppingProject/SignUpTest.java
@@ -1,13 +1,26 @@
 package E_Commerece.onLineShoppingProject;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.e_commerce.qa.base.TestBase;
+import com.e_commerce.qa.pages.HomePage;
 import com.e_commerce.qa.pages.SignIn;
 import com.e_commerce.qa.pages.SignUp;
 import com.e_commerce.qa.utils.ExcelUtils;
@@ -16,6 +29,9 @@ public class SignUpTest extends TestBase {
 
 	SignUp signUp;
 	SignIn signIn;
+	HomePage homepage;
+
+	WebDriverWait wait;
 
 	public SignUpTest() {
 		super();
@@ -35,11 +51,13 @@ public class SignUpTest extends TestBase {
 
 		signUp = new SignUp();
 
+		homepage = new HomePage();
+
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 	}
 
-	@Test(dataProvider = "customerDetails")
+	@Test(dataProvider = "customerDetailsFromOracleServerDatabase")
 	public void validateSignUpFormTest(String signUpEmail, String signUpUserFirstName, String signUpUserLastName,
 			String signUpPassword, String addessFirstName, String addressLastName, String address, String addressCity,
 			String addressZipCode, String addressMobilePhone, String commonName) {
@@ -52,15 +70,6 @@ public class SignUpTest extends TestBase {
 
 		signUp.selectMrs();
 
-		/*
-		 * signUp.fillForm(prop.getProperty("signUpUserFirstName"),
-		 * prop.getProperty("signUpUserLastName"), prop.getProperty("signUpPassword"),
-		 * prop.getProperty("addessFirstName"), prop.getProperty("addressLastName"),
-		 * prop.getProperty("address"), prop.getProperty("addressCity"),
-		 * prop.getProperty("addressZipCode"), prop.getProperty("addressMobilePhone"),
-		 * prop.getProperty("commonName"));
-		 */
-
 		signUp.fillForm(signUpUserFirstName, signUpUserLastName, signUpPassword, addessFirstName, addressLastName,
 				address, addressCity, addressZipCode, addressMobilePhone, commonName);
 
@@ -71,14 +80,18 @@ public class SignUpTest extends TestBase {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 		signUp.registerButtonClick();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//i[@class='icon-home']")).isDisplayed());
 
 	}
 
-	@DataProvider(name = "customerDetails")
-	String[][] dataProvider() throws Exception {
-		logger.info("Inside Customer Details Data Provider");
+	@DataProvider(name = "customerDetailsFromExcelSheet")
+	String[][] dataProviderofExcelsheet() throws Exception {
+		logger.info("Inside Customer Details Data Provider from excelsheet ");
 
-		String filePath = "C:\\Users\\Owner\\git\\E_commerce_automation_selenium\\onLineShoppingProject\\src\\main\\java\\com\\e_commerce\\qa\\testdata\\SignUpUser.xlsx";
+		String filePath = "C:\\Users\\Owner\\git\\E_commerce_automation_selenium\\onLineShoppingProject\\src\\main\\java\\com\\e_commerce\\qa\\testdata\\SignUpUser 1.xlsx";
 
 		int row = ExcelUtils.getRowCount(filePath, "Sheet1");
 		int col = ExcelUtils.getCellCount(filePath, "Sheet1", 1);
@@ -94,7 +107,51 @@ public class SignUpTest extends TestBase {
 
 		}
 
-		System.out.println("The data provided is: " + customerData.toString());
+		return customerData;
+
+	}
+
+	@DataProvider(name = "customerDetailsFromOracleServerDatabase")
+	String[][] dataProviderofOracleDataBase() throws SQLException {
+
+		logger.info("Inside Customer Details Data Provider from Oracle Database ");
+
+		Connection connection;
+
+		connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "hr", "Manvir1988");
+
+		Statement statement = connection.createStatement(java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE,
+				ResultSet.CONCUR_READ_ONLY);
+
+		String query = "select * from USERSIGNUPDETAILS";
+
+		ResultSet result;
+
+		result = statement.executeQuery(query);
+
+		int rowSize = 0;
+
+		result.last();
+		rowSize = result.getRow();
+		result.beforeFirst();
+
+		ResultSetMetaData rsmd = result.getMetaData();
+		int columnSize = rsmd.getColumnCount();
+
+		String[][] customerData = new String[rowSize][columnSize];
+
+		System.out.println("Total rows are :" + rowSize);
+
+		int i = 0;
+		while (result.next() && i < rowSize) {
+			for (int j = 0; j < columnSize; j++) {
+				customerData[i][j] = result.getString(j + 1);
+			}
+			i++;
+		}
+		result.close();
+		statement.close();
+		connection.close();
 
 		return customerData;
 
@@ -102,8 +159,6 @@ public class SignUpTest extends TestBase {
 
 	@AfterMethod
 	void tearDown() throws InterruptedException {
-
-		Thread.sleep(15000);
 
 		driver.quit();
 
